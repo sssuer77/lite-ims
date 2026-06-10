9eae8fa942cca43c392aee52cbec459560bb9c96
sssuer77 <otofoerster@gmail.com>
2026-06-10 12:40:11 +0800

    chore: 按照最新的 ignore 规则清理冗余文件

---
+++ backend/lite-ims/src/main/java/io/github/sssuer77/liteims/LiteImsApplication.java
@@ -0,0 +1,13 @@
+package io.github.sssuer77.liteims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteImsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteImsApplication.class, args);
+    }
+
+}
---
+++ backend/lite-ims/src/test/java/io/github/sssuer77/liteims/LiteImsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sssuer77.liteims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteImsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
